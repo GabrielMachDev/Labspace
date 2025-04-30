0978ecb972012a0f5b395f5d77f1b4d84f613f0f
GabrielMachDev <gabrielmachdev@outlook.com>
2025-04-30 08:19:42 -0300

    For - Loop and repetition structure

--- Python Fundamentals/python_for.py
+++ Python Fundamentals/python_for.py
@@ -1,19 +1,19 @@
-lista_vendas = [1000, 500, 800, 1500, 2000, 2300]
+sales_list = [1000, 500, 800, 1500, 2000, 2300]
 
-meta = 1200
-percentual_bonus = 0.1
+goal = 1200
+bonus_percent = 0.1
 
 
-for venda in lista_vendas:
-    if venda >= meta:
-        bonus = percentual_bonus * venda
+for sale in sales_list:
+    if sale >= goal:
+        bonus = bonus_percent * sale
     else:
         bonus = 0
     print(bonus)
 
 
-# for venda in lista_vendas:
-#     print(venda)
-#     print("próximo item")
+# for sale in sales_list:
+#     print(sale)
+#     print("Next item")
 
-# fora do for
+# out of the for
